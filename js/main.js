90acc2640fdcbef5ece6418fc116e1bf457f2e44
Celine Chen <cchen579@wisc.edu>
2020-12-10 18:07:02 -0800

    12/10/20 update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,63 @@
 
 //wrap everything in a self-executing anonymous function to move to local scope
-(function(){
-
-  //accordian panel
-  var allPanels = $('.accordion > dd').hide();
-    
-  $('.accordion > dt > a').click(function() {
-    allPanels.slideUp();
-    $(this).parent().next().slideDown();
-    return false;
+$(document).ready(function () {
+ 
+  var map = L.map('mapdiv', {
+    center: [39.73, -104.99],
+    zoom: 4
+    //layers: [osm, FracFocusPoints]
   });
 
-})(); //last line of main.js - self-executing anonymous function
+  var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+  // var baseMaps = {
+  //   "Open StreetMap": osm
+  // };
+  
+  // var overlayMaps = {
+  //   "Fracking_Locations": FracFocusPoints,
+  // };
+  
+  //L.control.layers(baseMaps).addTo(map);
+
+var markers = L.markerClusterGroup();
+
+//function getData(map){
+  $.getJSON("data/FracFocusPoints.geojson", function (data){			
+    //fracking point layer
+      var FracFocusPoints = L.geoJson(data, {
+        pointToLayer: function(feature,latlng) {
+          return L.circleMarker(latlng, {
+            radius:6,
+            opacity: .5,
+            color: "#000",
+            fillOpacity: 0.8
+          });
+        },
+
+        onEachFeature: function(feature,layer) {
+          layer._leaflet_id = feature.properties.WellName;
+
+          var popupContent = "<p>Well Name: <b>" + feature.properties.WellName;
+          
+          if (feature.properties && feature.properties.popupContent) {
+            popupContent += feature.properties.popupContent;
+          }
+            layer.bindPopup(popupContent);
+        }
+      });
+      markers.addLayer(FracFocusPoints);
+      map.addLayer(markers);
+    });
+    
+
+//end fracking points layer
+
+//layer control//
+
+
+
+});
+//last line of main.js - self-executing anonymous function
